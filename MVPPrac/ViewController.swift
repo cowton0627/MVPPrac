712344527f216a26a242397a325a70ea60d26f7e
鄭淳澧 <cowton0517@gmail.com>
2022-04-19 13:29:03 +0800

    Initial Commit

---
+++ MVPPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPPrac
+//
+//  Created by Chun-Li Cheng on 2022/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
